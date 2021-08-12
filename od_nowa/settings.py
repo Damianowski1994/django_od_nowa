360df282f0ac1b5575b6c407f1c67841717b32e2
Damian <damian.rutkiewicz@interia.pl>
2021-08-12 12:37:01 +0200

    ref

--- od_nowa/settings.py
+++ od_nowa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -25,10 +26,12 @@ SECRET_KEY = 'y$jhfux)^##wit*hau@hssi%#w&xuxsm9p!&r96h61qwzi)h$h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+PROJECT_DIR = os.path.dirname(BASE_DIR)
+
 ALLOWED_HOSTS = ["*"]
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Application definition
 
